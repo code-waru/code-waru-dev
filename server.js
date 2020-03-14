2e210ac1168cba472286bb2caf079a7a82353d2c
code waru <codewaru@gmail.com>
2020-03-14 14:48:01 +0900

    add

---
+++ server.js
@@ -0,0 +1,18 @@
+let bs = require("browser-sync").create();
+
+const paths = {
+  rootDir : "./src",
+  srcDir : "./src",
+  html: "./src/**/*.html"
+}
+
+bs.init({
+  ui: {
+      port: 3001
+  },
+  port: 3000,
+  server: paths.rootDir,
+  online: false,
+  open: false,
+  files: [paths.html]
+});
